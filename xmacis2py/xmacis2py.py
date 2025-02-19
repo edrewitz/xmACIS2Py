2e94601505d654267544e8d6944b8cee73e16af3
Eric Drewitz <101157849+edrewitz@users.noreply.github.com>
2025-02-19 03:45:04 -0800

    Update xmacis2py.py

--- xmacis2py/xmacis2py.py
+++ xmacis2py/xmacis2py.py
@@ -26,6 +26,21 @@ except Exception as e:
 
 def plot_temperature_summary(station, product_type, start_date=None, end_date=None):
 
+    r'''
+    This function plots a graphic showing the Temperature Summary for a given station for a given time period. 
+
+    Required Arguments:
+
+    1) station (String) - The identifier of the ACIS2 station. 
+    2) product_type (String or Integer) - The type of product. 'Past 7 Days' as a string or enter 7 for the same result. 
+       A value of 'custom' or 'Custom' will result in the user entering a custom start/stop date. 
+
+    Optional Arguments:
+    1) start_date (String) - Default=None. Enter the start date as a string (i.e. 01-01-2025)
+    2) end_date (String) - Default=None. Enter the end date as a string (i.e. 01-01-2025)
+
+    '''
+
     try:
         today = datetime.now(UTC)
     except Exception as e:
