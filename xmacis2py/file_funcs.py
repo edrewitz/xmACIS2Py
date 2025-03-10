561717e0b8374160cd47402a0c667a59a408588e
Eric Drewitz <101157849+edrewitz@users.noreply.github.com>
2025-03-09 21:26:47 -0700

    Update file_funcs.py

--- xmacis2py/file_funcs.py
+++ xmacis2py/file_funcs.py
@@ -1,8 +1,19 @@
 import os
-import warnings
-warnings.filterwarnings('ignore')
 
 def update_csv_file_paths(station, product_type):
+
+    r'''
+    This function creates the file path for the data files.
+
+    Required Arguments:
+
+    1) station (String) - The Station ID
+
+    2) product_type (String) - The type of summary (30 Day, 90 Day etc.)
+
+    Returns: A file path for the graphic to save: f:ACIS Data/{station}/{product_type}
+
+    '''
 
     if os.path.exists(f"ACIS Data"):
         pass
@@ -25,6 +36,21 @@ def update_csv_file_paths(station, product_type):
     return path, path_print
 
 def update_image_file_paths(station, product_type, plot_type):
+
+    r'''
+    This function creates the file path for the graphics files.
+
+    Required Arguments:
+
+    1) station (String) - The Station ID
+
+    2) product_type (String) - The type of summary (30 Day, 90 Day etc.)
+
+    3) plot_type (String) - The type of summary (i.e. temperature or precipitation)
+
+    Returns: A file path for the graphic to save: f:ACIS Graphics/{station}/{product_type}/{plot_type}
+
+    '''
 
     if os.path.exists(f"ACIS Graphics"):
         pass
