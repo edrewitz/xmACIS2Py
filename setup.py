10ab1d1a4432fb2c66c77f0f3c4fd9b6c9170375
Eric Drewitz <101157849+edrewitz@users.noreply.github.com>
2025-03-12 00:25:09 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if sys.version_info[0] < 3:
 
 setup(
     name = "xmacis2py",
-    version = "1.2.3",
+    version = "1.2.4",
     packages = find_packages(),
     install_requires=[
         "matplotlib>=3.7",
